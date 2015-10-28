f1bfff4d69c396767f5f8b071b5ab39b58097bce
Nelson <contact.nelsonic@gmail.com>
2015-10-28 06:44:10 +0000

    tidy up to simplify

--- google-apps-script.js
+++ google-apps-script.js
@@ -4,7 +4,7 @@
  * All credit still goes to Martin and any issues/complaints/questions to me. *
  ******************************************************************************/
 
-var TO_ADDRESS = "contact.nelsonic@gmail.com"; // where to send form data
+var TO_ADDRESS = "contact.nelsonic+form.submit@gmail.com"; // change this ...
 
 function doPost(e) {
 
