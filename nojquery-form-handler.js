db25950cb1dc2cb1942b125b72371d7aa4e121dd
Nelson <contact.nelsonic@gmail.com>
2015-10-28 06:34:07 +0000

    nojquery submission works as expected. removing old code

--- nojquery-form-handler.js
+++ nojquery-form-handler.js
@@ -1,70 +1,14 @@
-// Requested by Naza ... See: http://stackoverflow.com/a/46181
-// jQuery( document ).ready(function( $ ) {
-//     // variable to hold request
-//     var request;
-//     // bind to the submit event of our form
-//     $("#gform").submit(function(event){
-//         // prevent default posting of form
-//         event.preventDefault();
-//         var email = $('#email').val();
-//         console.log(' >> '+ email);
-//         if(!validateEmail(email)) { // validate the email address
-//           $('#email-invalid').show();
-//           return false;
-//         }
-//         // abort any pending request
-//         if (request) {
-//             request.abort();
-//         }
-//         var $form = $(this); // setup some local variables
-//         // let's select and cache all the fields
-//         var $inputs = $form.find("input, select, button, textarea");
-//         // serialize the data in the form
-//         var serializedData = $form.serialize();
-//         // disable the inputs for the duration of the ajax request
-//         $inputs.prop("disabled", true);
-//         // $('#gform').text('Sending data...');
-//         $('button').attr("disabled", "disabled");
-//         request = $.ajax({ // send the data to Google Apps
-//           url: event.target.action, // defined in the form action!
-//           type: "post",
-//           data: serializedData
-//         });
-//         // callback handler that will be called on success
-//         request.done(function (response, textStatus, jqXHR) {
-//           console.log(response, textStatus, jqXHR);
-//           $('#thankyou_message').show('slow');
-//           $('#gform').hide();
-//         });
-//         // callback handler that will be called on failure
-//         request.fail(function (jqXHR, textStatus, errorThrown){
-//             // log the error to the console
-//             console.error(
-//                 "The following error occured: "+
-//                 textStatus, errorThrown
-//             );
-//         });
-//         // callback handler that will be called regardless
-//         // if the request failed or succeeded
-//         request.always(function () {
-//             $inputs.prop("disabled", false);
-//         });
-//     });
-// });
 
-function validateEmail(email) {
+function validEmail(email) { // see:
   var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
   return re.test(email);
 }
 // get all data in form and return object
 function getFormData() {
   var data   = {
-    city         : document.getElementById("city").value,
-    start_date   : document.getElementsByName("start_date") > 0 ? document.getElementsByName("start_date")[1].value : '',
-    end_date     : document.getElementsByName("end_date")   > 0 ? document.getElementsByName("end_date")[1].value   : '',
-    sleeps       : document.getElementsByName("sleeps")         ? document.getElementsByName("sleeps")[0].value     : '',
-    email        : document.getElementById("email") ? document.getElementById("email").value : '',
-    phone_number : document.getElementById("phone") ? document.getElementById("phone").value : '8888888'
+    name    : document.getElementById("name").value,
+    email   : document.getElementById("email").value,
+    message : document.getElementById("message").value
   }
   console.log(data);
   return data;
@@ -98,6 +42,8 @@ function handleFormSubmit(event) {
   }
 }
 function loaded() {
+  console.log('loaded');
+  getFormData();
   // bind to the submit event of our form
   var form = document.getElementById('gform');
   form.addEventListener("submit", handleFormSubmit, false);
